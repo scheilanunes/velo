c1bf4c3cd8e89a0ee937969ea0a284369cfdbd16
Scheila Nunes <scheilacnunes19@gmail.com>
2026-02-06 17:42:22 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,21 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
+
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
-  
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-    //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-TZ231Q') 
   await page.getByTestId('search-order-button').click()
 
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-TZ231Q')
 
